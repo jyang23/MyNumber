9af0390733b4baa6e4b797d4df4f9615460b3532
GBTC408007UR <GBTC408007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 09:58:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int mynumber;
+        float myothernumber;
+
+        mynumber = 2;
+        myothernumber = 1.7938f;
+
+        System.out.println("My number is "+ mynumber);
+        System.out.println("My other number is "+ myothernumber);
+    }
+}
